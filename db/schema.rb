4fd3499f22e4e2c769c6b3ab51a99c671c124f8b
Alexander <alexander@Alex.local>
2020-08-07 13:48:20 +0700

    ahhh

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_043653) do
+ActiveRecord::Schema.define(version: 2020_08_07_052412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "commuities", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "communities", force: :cascade do |t|
     t.bigint "user_id"
@@ -52,6 +47,15 @@ ActiveRecord::Schema.define(version: 2020_08_07_043653) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["community_id"], name: "index_subscribers_on_community_id"
     t.index ["user_id"], name: "index_subscribers_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "community_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["community_id"], name: "index_subscriptions_on_community_id"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
