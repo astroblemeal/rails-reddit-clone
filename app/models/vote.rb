938f3cdb90f45c633a42c5ad3c4a18f189cc399b
Alexander <alexander@Alex.local>
2020-08-09 20:38:20 +0700

    votes fixed

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,12 +2,13 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates_uniqueness_of :user_id, scrope: :post_id
+  validates_uniqueness_of :user_id, scope: :post_id
 
     after_create :increment_vote
     after_destroy :decrement_vote
 
     private
+
     def increment_vote
       field = self.upvote ? :upvotes : :downvotes
       Post.find(self.post_id).increment(field).save
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   end
   resources :subscriptions
   resources :comments, only: [:create]
+
+  post "post/vote" => "votes#create"
+
   root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
