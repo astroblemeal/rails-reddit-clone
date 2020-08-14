4f35d109517a5724c429f02a64eedf971f30e29f
Alexander <alexander@Alex.local>
2020-08-14 14:26:17 +0700

    fixed it

--- db/migrate/20200806094019_add_username_to_user.rb
+++ db/migrate/20200806094019_add_username_to_user.rb
@@ -1,8 +1,5 @@
 class AddUsernameToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :username, :string
-    t.references :user
-    t.references :community
-    t.timestamps
   end
 end
